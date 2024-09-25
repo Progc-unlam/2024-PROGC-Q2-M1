b82b79ae1fadbe4ed430c04f4821eb7319740afc
EmaJuarezDev <emanuel.juarez1999@outlook.com>
2024-09-25 18:07:11 -0300

    Terminado Tp1-Part3 C++

--- TP1-parte3/01_C++/main.cpp
+++ TP1-parte3/01_C++/main.cpp
@@ -1,19 +1,20 @@
 #include <fcntl.h>
 #include <semaphore.h>
 #include <unistd.h>
+
+#include <cstdlib>
 #include <iostream>
 #include <list>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include <mutex>
-#include <cstdlib>
 
 using namespace std;
 
 #define ERROR_NPARAMETERS -1
 #define ERROR_NCLIENTS -2
 
-#define NADMITTED_PARAMTERS 2 
+#define NADMITTED_PARAMTERS 2
 #define NRESTOKERS 2
 #define MAX_BUY 2
 #define MIN_BUY 1
@@ -21,14 +22,15 @@ using namespace std;
 int nClients = 0;
 vector<sem_t *> semaphores;
 sem_t *reponerS;
-sem_t *waitRestock;  ///
+sem_t *waitRestock;
 mutex gondolaAccesMutex;
 mutex waitingArea;
-int productCount=10;
+int productCount = 10;
+bool noMoreClients = false;
 
 int parametersValidation(int nParamters, char const *paramters[]);
 list<thread> createRestockers(int nRestockers);
-void reponer(int numb_restocker,sem_t *myInit, sem_t *nextRepo);
+void reponer(int numb_restocker, sem_t *myInit, sem_t *nextRepo);
 list<thread> createClients(int nClients);
 void comprar(int order);
 
@@ -42,36 +44,38 @@ int main(int argc, char const *argv[])
     return validation;
   }
 
-  cout << "Good init" << endl;
   cout << "N clients - " << nClients << endl;
 
   reponerS = sem_open("reponer", O_CREAT, 0600, 0);
   waitRestock = sem_open("esperar", O_CREAT, 0600, 0);
-
+  waitingArea.lock();
   list<thread> listRestockers = createRestockers((NRESTOKERS));
   list<thread> listClients = createClients(nClients);
 
-  cout << "No empieza aun" << endl;
-  sleep(5);
-  sem_post(reponerS);
-  sleep(15);
-  sem_post(reponerS);
-
-  for(auto &client : listClients)
+  waitingArea.unlock();
+  for (auto &client : listClients)
   {
     client.join();
   }
+  cout << "Close SuperMarket" << endl;
+  noMoreClients = true;
   for (auto &restocker : listRestockers)
   {
     restocker.join();
   }
+  int indxNameSemaphore = 0;
+  string nameSemaphoreClose;
   for (auto &semaphore : semaphores)
   {
+    nameSemaphoreClose = "Número: " + std::to_string(indxNameSemaphore);
     sem_close(semaphore);
+    sem_unlink(nameSemaphoreClose.c_str());
+    indxNameSemaphore++;
   }
   sem_close(reponerS);
   sem_close(waitRestock);
-
+  sem_unlink("reponer");
+  sem_unlink("esperar");
   return 0;
 }
 
@@ -95,41 +99,51 @@ int parametersValidation(int nParamters, char const *paramters[])
 list<thread> createRestockers(int nRestockers)
 {
   list<thread> restockers;
+  string nameSemaphore;
   for (int i = 0; i < nRestockers; i++)
   {
-    string nameSemaphore = "Número: " + std::to_string(i);
-    semaphores.push_back(sem_open(nameSemaphore.c_str(), O_CREAT, 0600, i==0?1:0));
+    nameSemaphore = "Número: " + std::to_string(i);
+    semaphores.push_back(sem_open(nameSemaphore.c_str(), O_CREAT, 0600, i == 0 ? 1 : 0));
   }
   for (int i = 0; i < nRestockers; i++)
   {
-    restockers.push_back(std::thread(reponer,i+1, semaphores.at(i), semaphores.at(i + 1 < nRestockers ? i + 1 : 0)));
+    restockers.push_back(std::thread(reponer, i + 1, semaphores.at(i), semaphores.at(i + 1 < nRestockers ? i + 1 : 0)));
   }
 
   return restockers;
 }
 
-void reponer(int numb_restocker,sem_t *myInit, sem_t *nextRepo)
+void reponer(int numb_restocker, sem_t *myInit, sem_t *nextRepo)
 {
-  sem_wait(myInit);
-  sem_wait(reponerS);
-  gondolaAccesMutex.lock();
-  productCount+=10;
-  cout << numb_restocker << " XD" << endl;
-  cout << productCount << endl;
-  gondolaAccesMutex.unlock();
-  sem_post(nextRepo);
-  sem_post(waitRestock);
+  while (1)
+  {
+    while (sem_trywait(myInit) != 0)
+    {
+      sleep(5);
+      if (noMoreClients == true) return;
+    }
+    while (sem_trywait(reponerS) != 0)
+    {
+      sleep(1);
+      if (noMoreClients == true) return;
+    }
+    cout << "Restocker N°" << numb_restocker << " restocking" << endl;
+    gondolaAccesMutex.lock();
+    productCount = 10;
+    gondolaAccesMutex.unlock();
+    sem_post(nextRepo);
+    sem_post(waitRestock);
+  }
 }
 
 list<thread> createClients(int nClients)
 {
   list<thread> clients;
   int nRand;
-  //std::srand(static_cast<unsigned int>(std::time(0)));
-  for(int i=0; i < nClients; i++)
-  { 
+  for (int i = 0; i < nClients; i++)
+  {
     nRand = MIN_BUY + (std::rand() % (MAX_BUY - MIN_BUY + 1));
-    clients.push_back(std::thread(comprar,nRand));
+    clients.push_back(std::thread(comprar, nRand));
   }
 
   return clients;
@@ -137,24 +151,20 @@ list<thread> createClients(int nClients)
 
 void comprar(int order)
 {
-  waitingArea.lock(); 
+  waitingArea.lock();
   gondolaAccesMutex.lock();
-  cout << " I want "<< order << " products..."<< endl;
-  while(productCount <= order)
+  cout << " I want " << order << " products..." << endl;
+
+  if (productCount - order < 0)
   {
-    order-=productCount;
-    productCount=0;
-    cout << "   - I'll wait for "<< order << " more."<< endl;
+    cout << "   - I'll wait for " << order << " more." << endl;
     gondolaAccesMutex.unlock();
     sem_post(reponerS);
     sem_wait(waitRestock);
     gondolaAccesMutex.lock();
-  } 
-  if(order!=0 && productCount>order)
-  {
-    order-=productCount;
-    cout << " Thanks :D "<< endl;
   }
+  productCount -= order;
+  cout << " Thanks :D " << endl;
   gondolaAccesMutex.unlock();
   waitingArea.unlock();
 }
