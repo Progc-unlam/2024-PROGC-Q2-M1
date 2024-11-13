6b6108e32f555def169bf08a7242655e552610b9
cbrss <cberrioslima@alumno.unlam.edu.ar>
2024-11-13 16:29:11 -0300

    borrado de comentarios

--- TP1-parte3/03_Java/Bath.java
+++ TP1-parte3/03_Java/Bath.java
@@ -2,19 +2,15 @@ import java.util.concurrent.Semaphore;
 
 public class Bath implements Runnable 
 {
-    /* Constantes */
     private static final int TIME_IN_BATH = 5000;
     private static final int TIME_WAITING = 3000;
     private static final int BATH_CAPACITY = 5;
     private static final String MAN = "H";
     private static final String WOMAN = "M";
 
-
-    /* Variables propias de cada persona */
     private String gender;
     private int number;
 
-    /* Recursos compartidos */
     public static int women_counter = 0;
     public static int men_counter = 0;
     public static Semaphore sem_access_bath = new Semaphore(BATH_CAPACITY);
@@ -27,41 +23,28 @@ public class Bath implements Runnable
 
     public void use_bath() throws InterruptedException 
     {
-        /* Si un hombre quiere entrar y no hay mujeres en el baño, entonces pasa */
         if (this.gender.equals(MAN) && get_women_counter() == 0)
         {
-            /* Si es el primer hombre, entonces bloqueo el acceso a mujeres */
             Main.enter_bath(number);
-
-            /* Dibujo un hombre en pantalla */
             Bath.increment_men_counter();
 
-            /* Simula el tiempo que el hilo esta usando el baño */
             Thread.sleep(TIME_IN_BATH); 
             
             Main.leave_bath(number);
-            /* Borro al hombre en pantalla */
             Bath.decrement_men_counter();
-          
-           
         } 
-        /* Entra mujer y contador de hombres debe estar en 0 */
+
         else if (this.gender.equals(WOMAN) && get_men_counter() == 0)
         {
             Main.enter_bath(number);
-            
-            /* Dibujo una mujer en pantalla */
             Bath.increment_women_counter();
             
-            /* Simula el tiempo que el hilo esta usando el baño */
             Thread.sleep(TIME_IN_BATH); 
             
-            /* Se va y actualiza la pantalla */
             Main.leave_bath(number);
             Bath.decrement_women_counter();
         } else
         { 
-            /* Si no pudo ingresar sea hombre o mujer, se va un rato y vuelve a intentar (pero con su lugar reservado para evitar inanicion) */
             Thread.sleep(TIME_WAITING);
             use_bath();
         }
